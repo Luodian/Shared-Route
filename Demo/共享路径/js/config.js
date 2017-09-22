71a3bbd2d2eeadb779d81cc03d297e43aeebc9a6
Luodian <1121058986@qq.com>
2017-09-22 22:06:30 +0800

    update mock demo

---
+++ Demo/共享路径/js/config.js
@@ -0,0 +1,1 @@
+window.config={"logo":"","background":"#C4C4C4","shell":"iPhone","showControlPanel":true,"iOSGuide":true,"alwaysShowLinks":false,"hoverShowLinks":false,"hoverShowRemarks":true,"showComments":true,"showQRCode":false,"hintLinks":false,"scrollRemarks":true,"isShowAllPageNode":true}
