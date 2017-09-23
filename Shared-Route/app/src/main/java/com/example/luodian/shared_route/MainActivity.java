c446954b472fca57c740c4d2b76e8f1b449de53a
Luodian <1121058986@qq.com>
2017-09-23 21:54:35 +0800

    update discussion details

--- Shared-Route/app/src/main/java/com/example/luodian/shared_route/MainActivity.java
+++ Shared-Route/app/src/main/java/com/example/luodian/shared_route/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.luodian.shared_route;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView mTextView;
+    private BottomNavigationView mNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.text);
+        mNavigationView = (BottomNavigationView)findViewById(R.id.bnv_menu);
+        mNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        mTextView.setText(item.getTitle().toString().toUpperCase());
+                        return true;
+                    }
+                });
     }
 }
