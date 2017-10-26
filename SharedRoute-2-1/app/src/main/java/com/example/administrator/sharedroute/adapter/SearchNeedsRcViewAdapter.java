43ba8226ec069f81169c419c40df33c6e85569b5
Luodian <drluodian@gmail.com>
2017-10-26 08:04:52 +0800

    add refresh action to searchneeds activity

--- SharedRoute-2-1/app/src/main/java/com/example/administrator/sharedroute/adapter/SearchNeedsRcViewAdapter.java
+++ SharedRoute-2-1/app/src/main/java/com/example/administrator/sharedroute/adapter/SearchNeedsRcViewAdapter.java
@@ -20,8 +20,13 @@ import com.example.administrator.sharedroute.entity.listItem;
 import com.example.administrator.sharedroute.listener.OnBlurCompleteListener;
 import com.example.administrator.sharedroute.localdatabase.OrderDao;
 import com.example.administrator.sharedroute.widget.BlurBehind;
+import com.pnikosis.materialishprogress.ProgressWheel;
 
 import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import static com.example.administrator.sharedroute.activity.SearchNeedsActivity.selectedItem;
 
@@ -34,7 +39,10 @@ public class SearchNeedsRcViewAdapter extends RecyclerView.Adapter<SearchNeedsRc
     private CallBackListener mCallBackListener;
     private Context mContext;
     private OrderDao orderDao;
-    public static interface OnItemClickListener {
+    private final static int TYPE_FOOTER = 1;//底部--往往是loading_more
+    private final static int TYPE_NORMAL = 2; // 正常的一条文章
+
+    interface OnItemClickListener {
         void onItemClick(View view, int position);
     }
     private OnItemClickListener mOnItemClickListener = null;
@@ -102,76 +110,78 @@ public class SearchNeedsRcViewAdapter extends RecyclerView.Adapter<SearchNeedsRc
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-        if(mContext == null){
-            mContext = parent.getContext();
-        }
-        if (orderDao==null) orderDao = new OrderDao(mContext);
-        View view = LayoutInflater.from(mContext).inflate(R.layout.content_search_needs,parent,false);
-        final ViewHolder holder = new ViewHolder(view);
+        View view;
+        switch (viewType) {
+            //其他无法处理的情况使用viewholder_article_simple
+            default:
+            case TYPE_NORMAL:
+                if (mContext == null) {
+                    mContext = parent.getContext();
+                }
+                if (orderDao == null) orderDao = new OrderDao(mContext);
+                view = LayoutInflater.from(mContext).inflate(R.layout.content_search_needs, parent, false);
+                final ViewHolder holder = new ViewHolder(view);
 
-        holder.mCardView.setOnLongClickListener(new View.OnLongClickListener(){
-            @Override
-            public boolean onLongClick(View v){
-                BlurBehind.getInstance().execute((Activity) mContext, new OnBlurCompleteListener() {
-                    final int position = holder.getAdapterPosition();
+                holder.mCardView.setOnLongClickListener(new View.OnLongClickListener() {
                     @Override
-                    public void onBlurComplete() {
-                        Intent intent = new Intent(mContext, BlurredActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                        intent.putExtra("title_name","title"+Integer.toString(position+1));
-                        intent.putExtra("select","release");
-                        intent.putExtra("Activity","SearchNeeds");
-                        mContext.startActivity(intent);
+                    public boolean onLongClick(View v) {
+                        BlurBehind.getInstance().execute((Activity) mContext, new OnBlurCompleteListener() {
+                            final int position = holder.getAdapterPosition();
+
+                            @Override
+                            public void onBlurComplete() {
+                                Intent intent = new Intent(mContext, BlurredActivity.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                intent.putExtra("title_name", "title" + Integer.toString(position + 1));
+                                intent.putExtra("select", "release");
+                                intent.putExtra("Activity", "SearchNeeds");
+                                mContext.startActivity(intent);
+                            }
+                        });
+                        return true;
                     }
                 });
-                return true;
-            }
-        });
-        return holder;
+                return holder;
+            case TYPE_FOOTER:
+                view = LayoutInflater.from(mContext).inflate(
+                        R.layout.recyclerview_footer, parent, false);
+                return new FooterViewHolder(view);
+        }
     }
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position)
     {
+        if (holder instanceof FooterViewHolder) {
+            ((FooterViewHolder) holder).rcvLoadMore.spin();
+        }
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.kindsTextView.setText(mDataset.get(position).getExpressType());
-        holder.fetchTimeTextView.setText(mDataset.get(position).getInTimeStamp());
-        holder.sendTimeTextView.setText(mDataset.get(position).getOutTimeStamp());
-        holder.fetchLocTextView.setText(mDataset.get(position).getInLocation());
-        holder.sendLocTextView.setText(mDataset.get(position).getOutLocation());
-        holder.priceTextView.setText(String.valueOf(mDataset.get(position).getPrice())+"元");
+        else {
+            holder.kindsTextView.setText(mDataset.get(position).getExpressType());
+            holder.fetchTimeTextView.setText(mDataset.get(position).getInTimeStamp());
+            holder.sendTimeTextView.setText(mDataset.get(position).getOutTimeStamp());
+            holder.fetchLocTextView.setText(mDataset.get(position).getInLocation());
+            holder.sendLocTextView.setText(mDataset.get(position).getOutLocation());
+            holder.priceTextView.setText(String.valueOf(mDataset.get(position).getPrice()) + "元");
 
-        holder.mImageView.setOnClickListener(new View.OnClickListener() {
-            //说实话，这样监听不太好，每次滑动getView的时候都要重新new一个监听，但是必须获取ChechView所在的那个Item的position，所以只能卸载getView函数内部
-            @Override
-            public void onClick(View v) {//等于说对于那10个左右的ChechBox,其绑定的监听在不断的改变
-                if (holder.mImageView != null && mCallBackListener != null)
-                    mCallBackListener.callBackImg(holder.mImageView);
-                assert holder.mImageView != null;
-                holder.mImageView.setClickable(false);
-                holder.mImageView.setImageResource(R.drawable.trolley_pressed);
-                listItem item = mDataset.get(position);
-                item.setPickupCode(String.valueOf((int)(Math.random()*1000000)));
-                orderDao.insertData(item);
-                Log.e("rrr","!!!");
-                selectedItem.add(item);
-            }
-        });
-//        holder.mCardView.setOnLongClickListener(new View.OnLongClickListener(){
-//            @Override
-//            public boolean onLongClick(View v){
-//                BlurBehind.getInstance().execute((Activity) mContext, new OnBlurCompleteListener() {
-//                    @Override
-//                    public void onBlurComplete() {
-//                        Intent intent = new Intent(mContext, BlurredActivity.class);
-//                        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-//                        mContext.startActivity(intent);
-//                    }
-//                });
-//                return true;
-//            }
-//        });
+            holder.mImageView.setOnClickListener(new View.OnClickListener() {
+                //说实话，这样监听不太好，每次滑动getView的时候都要重新new一个监听，但是必须获取ChechView所在的那个Item的position，所以只能卸载getView函数内部
+                @Override
+                public void onClick(View v) {//等于说对于那10个左右的ChechBox,其绑定的监听在不断的改变
+                    if (holder.mImageView != null && mCallBackListener != null)
+                        mCallBackListener.callBackImg(holder.mImageView);
+                    assert holder.mImageView != null;
+                    holder.mImageView.setClickable(false);
+                    holder.mImageView.setImageResource(R.drawable.trolley_pressed);
+                    listItem item = mDataset.get(position);
+                    item.setPickupCode(String.valueOf((int) (Math.random() * 1000000)));
+                    orderDao.insertData(item);
+                    Log.e("rrr", "!!!");
+                    selectedItem.add(item);
+                }
+            });
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
@@ -187,5 +197,18 @@ public class SearchNeedsRcViewAdapter extends RecyclerView.Adapter<SearchNeedsRc
 
     public interface CallBackListener{
         void callBackImg(ImageView goodsImg);
+    }
+
+    /**
+     * 底部加载更多
+     */
+    class FooterViewHolder extends SearchNeedsRcViewAdapter.ViewHolder {
+        @BindView(R.id.rcv_load_more)
+        ProgressWheel rcvLoadMore;
+
+        FooterViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
     }
 }
