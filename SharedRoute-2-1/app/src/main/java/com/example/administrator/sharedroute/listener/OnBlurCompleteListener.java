b765da3a80b8a54138e8bb8c5a2866b673660913
Luodian <1121058986@qq.com>
2017-11-04 08:48:42 +0800

    修复master

--- SharedRoute-2-1/app/src/main/java/com/example/administrator/sharedroute/listener/OnBlurCompleteListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.administrator.sharedroute.listener;
-
-/**
- * Created by gergun on 13/05/15.
- */
-public interface OnBlurCompleteListener {
-
-    public void onBlurComplete();
-}
