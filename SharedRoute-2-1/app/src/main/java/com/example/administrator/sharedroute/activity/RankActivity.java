af2cad72bd6ddf5f669add543b4be2a0beeb9913
BIGKnight <309426989@qq.com>
2017-12-06 21:29:49 +0800

    完成了排行和服务器的交接，但是还可以优化一下

--- SharedRoute-2-1/app/src/main/java/com/example/administrator/sharedroute/activity/RankActivity.java
+++ SharedRoute-2-1/app/src/main/java/com/example/administrator/sharedroute/activity/RankActivity.java
@@ -1,12 +1,16 @@
 package com.example.administrator.sharedroute.activity;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,21 +20,48 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.administrator.sharedroute.R;
 import com.example.administrator.sharedroute.entity.Client;
+import com.example.administrator.sharedroute.entity.listItem;
 import com.nhaarman.listviewanimations.ArrayAdapter;
+import com.nhaarman.listviewanimations.appearance.simple.SwingBottomInAnimationAdapter;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
+import java.util.List;
 
 public class RankActivity extends AppCompatActivity {
     private ListView mListView;
-    private SwipeRefreshLayout mSwipe;
     private ArrayList<Client> arrayList;
+    public static String PorA = "submit";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rank);
+
+
+
+
+
+
+
+
+        setContentView( R.layout.activity_rank2 );
         View decorView = getWindow().getDecorView();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -44,22 +75,102 @@ public class RankActivity extends AppCompatActivity {
                     .LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
         initView();
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarss);
+        setSupportActionBar(toolbar);
+        if (PorA.equals("submit"))getSupportActionBar().setTitle("发单排行");
+        else if (PorA.equals("fetch"))getSupportActionBar().setTitle("接单排行");
     }
-
     private void initView() {
-        mListView = (ListView)findViewById(R.id.rand_list);
-        mSwipe = (SwipeRefreshLayout)findViewById(R.id.rand_swipe);
-        RankAdapter rankAdapter = new RankAdapter(RankActivity.this);
-        rankAdapter.add(new Client("1150310116","李博",15,12));
-        rankAdapter.add(new Client("1150310116","武德浩",14,11));
-        rankAdapter.add(new Client("1150310116","王烨臻",13,11));
-        mListView.setAdapter(rankAdapter);
+        new RandAsysc().execute();
     }
-    private class RandAsysc extends AsyncTask<Void,Void,Void>{
+    private class RandAsysc extends AsyncTask<Void,Void,Void> {
         @Override
         protected Void doInBackground(Void... voids) {
+            String result = null;
+//            String path = "http://47.95.194.146:8080/sharedroot_server/Task";
+            String path = getResources().getString(R.string.url)+"/Task";
+            HttpURLConnection con = null;
+            InputStream in = null;
+            try {
+                HttpClient client = new DefaultHttpClient();
+                HttpPost post = new HttpPost(path);
+
+
+                List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+                parameters.add(new BasicNameValuePair("action", "rank"));
+                parameters.add(new BasicNameValuePair("parameter",PorA));
+//                SharedPreferences sp = getSharedPreferences("now_account", Context.MODE_PRIVATE);
+//                String stuNum=sp.getString("now_stu_num",null);
+//                parameters.add(new BasicNameValuePair("PublisherID", stuNum));
+                UrlEncodedFormEntity ent = new UrlEncodedFormEntity(parameters, HTTP.UTF_8);
+                post.setEntity(ent);
+                HttpResponse responsePOST = client.execute(post);
+                HttpEntity resEntity = responsePOST.getEntity();
+                if (resEntity != null) {
+                    result = EntityUtils.toString(resEntity);
+                }
+                JSONArray arr = new JSONArray(result.toString());
+                arrayList = new ArrayList<Client>();
+                for (int i = 0; i < arr.length(); i++) {
+                    JSONObject lan = arr.getJSONObject(i);
+
+                    String id =  lan.getString("UserID");
+                    String name = lan.getString("Name");
+                    int publishtasknum = lan.getInt("PublishTaskNum");
+                    int fetchtasknum = lan.getInt("FetchTaskNum");
+                    Client client1 = new Client(id,name,fetchtasknum,publishtasknum);
+                    arrayList.add(client1);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             return null;
         }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            mListView = findViewById(R.id.rand_list);
+            RankAdapter rankAdapter = new RankAdapter(RankActivity.this);
+            for (Client c:arrayList) {
+                rankAdapter.add(c);
+            }
+            if (arrayList.size() == 0) Toast.makeText(RankActivity.this, "发生未知错误",Toast.LENGTH_SHORT).show();
+            SwingBottomInAnimationAdapter swingBottomInAnimationAdapter = new SwingBottomInAnimationAdapter(rankAdapter);
+            swingBottomInAnimationAdapter.setAbsListView(mListView);
+            mListView.setAdapter(swingBottomInAnimationAdapter);
+        }
+    }
+    public void setListViewHeightBasedOnChildren(ListView listView) {
+        // 获取ListView对应的Adapter
+        RankAdapter listAdapter = (RankAdapter) listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0, len = listAdapter.getCount(); i < len; i++) {
+            // listAdapter.getCount()返回数据项的数目
+            View listItem = listAdapter.getView(i, null, listView);
+            // 计算子项View 的宽高
+            listItem.measure(0, 0);
+            // 统计所有子项的总高度
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight+ (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        // listView.getDividerHeight()获取子项间分隔符占用的高度
+        // params.height最后得到整个ListView完整显示需要的高度
+        listView.setLayoutParams(params);
     }
 }
 class RankAdapter extends ArrayAdapter<Client> {
