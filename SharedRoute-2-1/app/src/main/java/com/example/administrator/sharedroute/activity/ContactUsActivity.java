3031f201247114e7a952feb50dade086732dc4f5
Luodian <drluodian@gmail.com>
2017-12-10 01:03:55 +0800

    导入了联系我们的接口

--- SharedRoute-2-1/app/src/main/java/com/example/administrator/sharedroute/activity/ContactUsActivity.java
+++ SharedRoute-2-1/app/src/main/java/com/example/administrator/sharedroute/activity/ContactUsActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import com.example.administrator.sharedroute.R;
 
 public class ContactUsActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
