70c67ac3e1868352ef621e4491ff8e83c7c4cf0c
PracNeyman <1799022371@qq.com>
2017-12-10 11:03:20 +0800

    add一些之后加入的资源文件

---
+++ SharedRoute-2-1/app/src/main/java/com/example/administrator/sharedroute/fragment/Fragment_campus_two.java
@@ -0,0 +1,229 @@
+package com.example.administrator.sharedroute.fragment;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.administrator.sharedroute.R;
+import com.example.administrator.sharedroute.adapter.SortAdapter;
+import com.example.administrator.sharedroute.entity.CharacterParser;
+import com.example.administrator.sharedroute.entity.PinyinComparator;
+import com.example.administrator.sharedroute.entity.SortModel;
+import com.example.administrator.sharedroute.widget.ClearEditText;
+import com.example.administrator.sharedroute.widget.SideBar;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Fragment_campus_two extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private ListView sortListView;
+    private SideBar sideBar;
+    private TextView dialog;
+    private SortAdapter adapter;
+    private ClearEditText mClearEditText;
+    final static private int requestCode = 1;
+    private PinyinComparator pinyinComparator;
+
+    private CharacterParser characterParser;
+    private List<SortModel> SourceDateList;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public Fragment_campus_two() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Fragment2.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Fragment_campus_two newInstance(String param1, String param2) {
+        Fragment_campus_two fragment = new Fragment_campus_two();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initViews();
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    private void initViews() {
+        characterParser = CharacterParser.getInstance();
+
+        pinyinComparator = new PinyinComparator();
+
+
+        sideBar = (SideBar) getView().findViewById(R.id.sidrbar);
+        dialog = (TextView) getView().findViewById(R.id.dialog);
+
+        sideBar.setTextView(dialog);
+
+        sideBar.setOnTouchingLetterChangedListener(new SideBar.OnTouchingLetterChangedListener() {
+
+            @Override
+            public void onTouchingLetterChanged(String s) {
+                int position = adapter.getPositionForSection(s.charAt(0));
+                if(position != -1){
+                    sortListView.setSelection(position);
+                }
+
+            }
+        });
+
+        sortListView = (ListView) getView().findViewById(R.id.country_lvcountry);
+        sortListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Intent intent = new Intent();
+                intent.putExtra("pickupLocation",((SortModel)adapter.getItem(position)).getName());
+                getActivity().setResult(requestCode,intent);
+                getActivity().finish();
+            }
+        });
+
+        SourceDateList = filledData(getResources().getStringArray(R.array.data2));//建立一张排序表
+
+        Collections.sort(SourceDateList, pinyinComparator);
+        adapter = new SortAdapter(getActivity(), SourceDateList);//建立一个排序适配器，并将SourceDateList加入其中
+        sortListView.setAdapter(adapter);//将适配器加入listView，SourceDataList中的内容将显示在sortListView中
+
+
+//        mClearEditText = (ClearEditText) getView().findViewById(R.id.filter_edit);
+//
+//        mClearEditText.addTextChangedListener(new TextWatcher() {
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                filterData(s.toString());
+//            }
+//
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count,
+//                                          int after) {
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//            }
+//        });
+    }
+
+
+
+    private List<SortModel> filledData(String [] date){
+        List<SortModel> mSortList = new ArrayList<SortModel>();
+
+        for(int i=0; i<date.length; i++){
+            SortModel sortModel = new SortModel();
+            sortModel.setName(date[i]);
+            String pinyin = characterParser.getSelling(date[i]);
+            String sortString = pinyin.substring(0, 1).toUpperCase();
+
+            if(sortString.matches("[A-Z]")){
+                sortModel.setSortLetters(sortString.toUpperCase());
+            }else{
+                sortModel.setSortLetters("#");
+            }
+
+            mSortList.add(sortModel);
+        }
+        return mSortList;
+
+    }
+
+
+    private void filterData(String filterStr){
+        List<SortModel> filterDateList = new ArrayList<SortModel>();
+
+        if(TextUtils.isEmpty(filterStr)){
+            filterDateList = SourceDateList;
+        }else{
+            filterDateList.clear();
+            for(SortModel sortModel : SourceDateList){
+                String name = sortModel.getName();
+                if(name.indexOf(filterStr.toString()) != -1 || characterParser.getSelling(name).startsWith(filterStr.toString())){
+                    filterDateList.add(sortModel);
+                }
+            }
+        }
+
+        Collections.sort(filterDateList, pinyinComparator);
+        adapter.updateListView(filterDateList);
+    }
+}
